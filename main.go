532a94790f372b4686da05f739d267c06d8f16b8
root <root@wien1.exploro.one>
2025-12-19 17:02:19 +0000

    fix printing issue

--- main.go
+++ main.go
@@ -150,9 +150,11 @@ func encResp(req *http.Request, w http.ResponseWriter, resp interface{}) error {
 	if strings.HasPrefix(accept, "text/plain") {
 		if s, ok := resp.(string); ok {
 			fmt.Fprintf(w, "%s", s)
+			return nil
 		}
 		if f, ok := resp.(Formattable); ok {
 			fmt.Fprintf(w, "%s", f.String())
+			return nil
 		}
 		return json.NewEncoder(w).Encode(resp)
 	}
